0ec9a3993cbe91d39a0fb41f5a9ac72033d7467c
MOHAMMED RAFHATH <danarafha@gmail.com>
2023-10-06 21:34:54 +0530

    modied schemas and some updations

--- routes/hotels.js
+++ routes/hotels.js
@@ -1,22 +1,24 @@
 import express from "express";
-import Hotel from "../models/Hotel.js";
-import { createError } from "../utils/error.js";
 import {
+  countByCity,
   createHotel,
   deleteHotel,
   getHotel,
   getHotels,
   updatedHotel,
 } from "../controllers/hotel.js";
+import { verifyAdmin } from "../utils/verifyToken.js";
 const router = express.Router();
 //create
-router.post("/", createHotel);
+router.post("/", verifyAdmin, createHotel);
 //update
-router.put("/:id", updatedHotel);
+router.put("/:id", verifyAdmin, updatedHotel);
 //delete
-router.delete("/:id", deleteHotel);
+router.delete("/find/:id", verifyAdmin, deleteHotel);
 //get
 router.get("/:id", getHotel);
 //getall
 router.get("/", getHotels);
+router.get("/countByCity", countByCity);
+router.get("/countByType", getHotels);
 export default router;
--- routes/rooms.js
+++ routes/rooms.js
@@ -1,4 +1,21 @@
 import express from "express";
+import { verifyAdmin } from "../utils/verifyToken.js";
+import {
+  createRoom,
+  deleteRoom,
+  getRoom,
+  getRooms,
+  updatedRoom,
+} from "../controllers/room.js";
 const router = express.Router();
-
+//create
+router.post("/:hotelid", verifyAdmin, createRoom);
+//update
+router.put("/:id", verifyAdmin, updatedRoom);
+//delete
+router.delete("/:id/:hotelid", verifyAdmin, deleteRoom);
+//get
+router.get("/:id", getRoom);
+//getall
+router.get("/", getRooms);
 export default router;
