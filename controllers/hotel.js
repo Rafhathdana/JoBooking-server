bce29494ec589e1b153a9fcaec6d14a32e8abe60
MOHAMMED RAFHATH <danarafha@gmail.com>
2023-10-12 23:16:37 +0530

    fixed and added some

--- controllers/hotel.js
+++ controllers/hotel.js
@@ -41,8 +41,13 @@ export const getHotel = async (req, res, next) => {
 };
 
 export const getHotels = async (req, res, next) => {
+  const { min, max, ...others } = req.query;
+  console.log(min, max);
   try {
-    const hotels = await Hotel.find(req.params.id);
+    const hotels = await Hotel.find({
+      ...others,
+      cheapestPrice: { $gt: min || 1, $lt: max || 999 },
+    }).limit(req.query.limit);
     res.status(200).json(hotels);
   } catch (err) {
     next(err);
